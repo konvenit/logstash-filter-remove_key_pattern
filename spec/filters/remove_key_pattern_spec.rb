a63097f1dee7c3630394ef2f12771b6687ea0077
Arthur Alfredo <arthurbalfredo@gmail.com>
2017-03-07 17:13:03 -0300

    Add test

--- spec/filters/remove_key_pattern_spec.rb
+++ spec/filters/remove_key_pattern_spec.rb
@@ -92,7 +92,51 @@ describe LogStash::Filters::RemoveKeyPattern do
       insist { subject.get("haystack2") }.include?("key")
       insist { subject.get("haystack2") }.include?("foobar")
     end
-
   end
+
+  describe "try to remove a pattern from unexisting parent keys" do
+    config <<-CONFIG
+      filter {
+        remove_key_pattern {
+          parent_keys => ["h2", "h1"]
+          pattern => ["needle", "\\d"]
+          keep_only_ids => "true"
+        }
+      }
+    CONFIG
+
+    hash = {
+      "hello" => {
+        "numbers" => ["one", "two", "three"],
+        "47" => ["2", "3", "7"],
+      },
+      "haystack" => {
+        "key1_id" => ["value1", "value2", "value3"],
+        "id" => ["value1", "value2", "value3"],
+        "key4_id" => ["value1", "value2", "value3"],
+        "key_four_id" => ["value1", "value2", "value3"],
+        "needle" => ["value1", "value2", "value3"],
+      },
+      "haystack2" => {
+        "key_one_id" => ["value1", "value2", "value3"],
+        "id" => ["value1", "value2", "value3"],
+        "key4_id" => ["value1", "value2", "value3"],
+        "key" => ["value1", "value2", "value3"],
+        "foobar" => ["value1", "value2", "value3"],
+      }
+    }
+
+    sample(hash) do
+      insist { subject.get("hello") }.include?("numbers")
+      insist { subject.get("haystack") }.include?("id")
+      insist { subject.get("haystack") }.include?("key_four_id")
+      insist { subject.get("haystack") }.include?("key4_id")
+      insist { subject.get("haystack2") }.include?("key_one_id")
+      insist { subject.get("haystack2") }.include?("key4_id")
+      insist { subject.get("haystack2") }.include?("key")
+      insist { subject.get("haystack2") }.include?("foobar")
+    end
+  end
+
 
 end
